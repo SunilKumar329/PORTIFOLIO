b884530a869a9b414f2dd146c0985ec2b4d81acd
SunilKumar329 <tskumar329@gmail.com>
2023-09-30 09:44:59 +0530

    new cha

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Experiencepage from './pages/Experiencepage/Experiencepage';
@@ -8,19 +8,18 @@ import Skillspage from './pages/Skillpage/Skillpage';
 import Mainnav from './components/Mainnav/Mainnav';
 import { TypeAnimation } from 'react-type-animation'; 
 import Contactpage from './pages/Contactpage/Contactpage';
-import { Home } from '@mui/icons-material';
+
 function HomePage() {
   return ( 
     <div>
       <Navbar />
-        {/* <h1 className='app-header'>Hello, I'm Sunil Kumar, a Fullstack Developer, and this is my Portfolio</h1> */}
-        <TypeAnimation
+      <TypeAnimation
         className='app-header'
-  sequence={["Hello, I'm Sunil Kumar, a Fullstack Developer, and this is my Portfolio...", 1000, ' ', 500]}
-  style={{ fontSize: '2em' }}
-  speed={55}
-  repeat={Infinity}
-/>
+        sequence={["Hello, I'm Sunil Kumar, a Fullstack Developer, and this is my Portfolio...", 1000, ' ', 500]}
+        style={{ fontSize: '2em' }}
+        speed={55}
+        repeat={Infinity}
+      />
       <Aboutpage />
       <Experiencepage />
       <Skillspage />
@@ -33,12 +32,11 @@ function App() {
     <Router>
       <div className="App">
         <Mainnav />
-        <HomePage />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<Aboutpage />} /> {/* Home page */}
-          <Route path="/experience" element={<Experiencepage />} /> {/* Experience page */}
-          <Route path="/skills" element={<Skillspage />} /> {/* Skills page */}
+          <Route path="/about" element={<Aboutpage />} />
+          <Route path="/experience" element={<Experiencepage />} />
+          <Route path="/skills" element={<Skillspage />} />
           <Route path="/contact" element={<Contactpage />} />
         </Routes>
       </div>
